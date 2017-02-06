79f013adaace37a69d3a2c26c5bdbd5f0d1c1df2
Alexandre Pujol <alexandre@pujol.io>
2017-02-06 16:00:51 +0000

    Update header

--- import.bash
+++ import.bash
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
-
-# Copyright (C) 2017 Alexandre Pujol <alexandre@pujol.io>. All Rights Reserved.
-# This file is licensed under the GPLv2+. Please see COPYING for more information.
-
-# Import manager - Password Store Extenssion (https://www.passwordstore.org/)
+# Import manager - Password Store Extension (https://www.passwordstore.org/)
+# Copyright (C) 2017 Alexandre PUJOL <alexandre@pujol.io>.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# 
 
 IMPORTER_DIR="${PASSWORD_STORE_IMPORTER_DIR:-/usr/lib/password-store/importers}"
 IMPORTERS=( "1password" "fpm" "gorrilla" "kedpm" "keepass" "keepass2csv"
