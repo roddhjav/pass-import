b2ddfe1e05639067a80c67339c39f6758f30e691
Alexandre Pujol <alexandre@pujol.io>
2017-08-12 20:04:31 +0100

    Add simple password store wrapper in python

--- lib/import.py
+++ lib/import.py
@@ -66,3 +66,70 @@ def die(msg):
     error(msg)
     exit(1)
 
+class PasswordStoreError(Exception):
+    pass
+
+class PasswordStore():
+    """ Simple Password Store for python, only able to insert password.
+        Supports all the environnement variables.
+    """
+    def __init__(self):
+        self.passbinary = "/usr/bin/pass"
+        self.env = dict(**os.environ)
+        self._setenv('PASSWORD_STORE_DIR', 'PREFIX')
+        self._setenv('PASSWORD_STORE_KEY')
+        self._setenv('PASSWORD_STORE_GIT', 'GIT_DIR')
+        self._setenv('PASSWORD_STORE_GPG_OPTS')
+        self._setenv('PASSWORD_STORE_X_SELECTION', 'X_SELECTION')
+        self._setenv('PASSWORD_STORE_CLIP_TIME', 'CLIP_TIME')
+        self._setenv('PASSWORD_STORE_UMASK')
+        self._setenv('PASSWORD_STORE_GENERATED_LENGHT', 'GENERATED_LENGTH')
+        self._setenv('PASSWORD_STORE_CHARACTER_SET', 'CHARACTER_SET')
+        self._setenv('PASSWORD_STORE_CHARACTER_SET_NO_SYMBOLS',
+                     'CHARACTER_SET_NO_SYMBOLS')
+        self._setenv('PASSWORD_STORE_ENABLE_EXTENSIONS')
+        self._setenv('PASSWORD_STORE_EXTENSIONS_DIR', 'EXTENSIONS')
+        self._setenv('PASSWORD_STORE_SIGNING_KEY')
+        self._setenv('GNUPGHOME')
+        self.prefix = self.env['PASSWORD_STORE_DIR']
+        self.exist = True  # FIXME
+
+    def _setenv(self, var, env = None):
+        """ Add var in the environnement variables directory.
+            env must be an existing os environnement variables.
+        """
+        if env is None:
+            env = var
+        if env in os.environ:
+            self.env[var] = os.environ[env]
+
+    def _pass(self, arg = None, data = None):
+        """ Call to password store """
+        command = [self.passbinary]
+        if arg is not None:
+            command.extend(arg)
+
+        process = Popen(command, universal_newlines = True, env = self.env,
+                        stdin = PIPE, stdout = PIPE, stderr = PIPE)
+        (stdout, stderr) = process.communicate(data)
+        res = process.wait()
+        if res:
+            raise PasswordStoreError("%s %s %s" % (command, stdout, stderr))
+
+        return stdout
+
+    def _add_to(self, arg, option, optname, boolean = False):
+        if option:
+            if boolean is False:
+                optname += '=' + option
+            arg.append(optname)
+        return arg
+
+    def insert(self, path, data, force = False):
+        """ Multiline insertion into the password store. """
+        arg = ['insert']
+        arg = self._add_to(arg, True, '--multiline', True)
+        arg = self._add_to(arg, force, '--force', True)
+        arg.append(path)
+        return self._pass(arg, data)
+
