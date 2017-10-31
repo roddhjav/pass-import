23be55914513271ef380dd0a08c3570f4ef9ec05
Alexandre Pujol <alexandre@pujol.io>
2017-10-31 23:42:03 +0000

    Tests: update bash tests with last changes

--- tests/40_import.sh
+++ tests/40_import.sh
@@ -5,14 +5,17 @@ export test_description="Testing 'pass import'"
 source ./setup
 test_cleanup
 
-PASSWORDS_MANAGERS=("1password" "1password4" "chrome" "dashlane" "enpass" "fpm"
-    "gorilla" "keepass" "keepassx" "keepasscsv"
-    "lastpass"  "passwordexporter" "pwsafe" "revelation" "roboform")
+PASSWORDS_MANAGERS=("1password" "1password4" "chrome" "dashlane" "enpass"
+    "fpm" "gorilla" "kedpm" "keepass" "keepassx" "keepassxc" "keepasscsv"
+    "lastpass" "passwordexporter" "pwsafe" "revelation" "roboform")
+
+XML="fpm keepassx keepass pwsafe revelation kedpm"
 
 for manager in "${PASSWORDS_MANAGERS[@]}"; do
 	test_init "$manager" &> /dev/null
+    _in "$XML" "$manager" && ext="xml" || ext="csv"
 	test_expect_success "Testing $manager database" "
-        _pass import $manager $PLAIN_DB/$manager
+        _pass import $manager $PLAIN_DB/$manager.$ext
     "
 done
 
