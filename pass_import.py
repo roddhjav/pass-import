c495b269ba1a1a6e87ef71780164b0a9bfeb553e
Alexandre Pujol <alexandre@pujol.io>
2019-05-05 19:42:30 +0100

    Remove useless if statement

--- pass_import.py
+++ pass_import.py
@@ -840,8 +840,6 @@ class Keepass(KeepassX):
             title = cls._getvalue(element.findall('String'), 'Title')
         elif element.tag == 'Group':
             title = element.find('Name').text
-        if title is None:
-            title = ''
         return os.path.join(path, title)
 
 
