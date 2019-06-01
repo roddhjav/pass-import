f321dabbb0ea660ec5adb597772455cebb744037
Alexandre Pujol <alexandre@pujol.io>
2019-06-01 17:58:35 +0100

    Fix classifiers.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
