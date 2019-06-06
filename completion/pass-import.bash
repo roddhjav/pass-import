283967a0a4fd93ce7b61a3c00d3b13228ff81511
Alexandre Pujol <alexandre@pujol.io>
2019-06-06 23:09:24 +0100

    Update completion to the last changes.

--- completion/pass-import.bash
+++ completion/pass-import.bash
@@ -3,12 +3,15 @@
 PASSWORD_STORE_EXTENSION_COMMANDS+=(import)
 
 __password_store_extension_complete_import() {
-	local importers=(1password 1password4pif apple-keychain bitwarden chrome
-		chromesqlite dashlane enpass fpm gorilla kedpm keepass keepasscsv
-		keepassx keepassx2 keepassxc lastpass networkmanager passwordexporter
-		pwsafe revelation roboform upm)
-	local args=(-h --help -p --path -a --all -c --clean -C --convert -s
-		--separator  -l --list -f --force -q --quiet -v --verbose -V --version)
+	local importers=(1password 1password4 1password4pif aegis andotp
+		apple-keychain bitwarden buttercup chrome chromesqlite csv dashlane
+		encryptr enpass enpass6 fpm gnome-authenticator gnome-keyring gorilla
+		kedpm keepass keepass-csv keepass-xml keepassx keepassx2 keepassx2-csv
+		keepassxc keepassxc-csv keeper lastpass networkmanager myki pass
+		passpie passwordexporter pwsafe revelation roboform upm)
+	local args=(-h --help -p --path -a --all -c --clean -C --convert -s --sep
+		  --cols --config -l --list -f --force -q --quiet -v --verbose -V
+		  --version)
 	local lastarg="${COMP_WORDS[$COMP_CWORD-1]}"
 	if [[ $lastarg == "-p" || $lastarg == "--path" ]]; then
 		_pass_complete_folders
