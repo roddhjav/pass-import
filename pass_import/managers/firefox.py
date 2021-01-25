e5eb33c69de3cd78db3762af13a7825f0c8eb0d6
Alexandre Pujol <alexandre@pujol.io>
2021-01-25 13:12:42 +0000

    Add support for modern firefox import.

--- pass_import/managers/firefox.py
+++ pass_import/managers/firefox.py
@@ -6,13 +6,30 @@ from pass_import.core import register_managers
 from pass_import.formats.csv import CSV
 
 
+class Firefox(CSV):
+    """Importer for Firefox in CSV format."""
+    name = 'firefox'
+    url = 'https://www.mozilla.org/en-US/firefox/lockwise/'
+    hexport = 'In about:logins Menu: Export logins'
+    himport = 'pass import firefox file.csv'
+    keys = {'title': 'url', 'password': 'password', 'login': 'username'}
+
+    @classmethod
+    def header(cls):
+        """Header for Firefox CSV file."""
+        return list(cls.keys.values()) + ['httpRealm', 'formActionOrigin',
+                                          'timeCreated', 'timeLastUsed',
+                                          'timePasswordChanged', 'guid']
+
+
 class FirefoxPasswordExporter(CSV):
     """Importer for Firefox password exporter extension in CSV format."""
     name = 'firefox'
+    default = False
     url = 'https://github.com/kspearrin/ff-password-exporter'
     hexport = 'Add-ons Prefs: Export Passwords: CSV'
     himport = 'pass import firefox file.csv'
     keys = {'title': 'hostname', 'password': 'password', 'login': 'username'}
 
 
-register_managers(FirefoxPasswordExporter)
+register_managers(Firefox, FirefoxPasswordExporter)
