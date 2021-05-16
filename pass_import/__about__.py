2d3567c2f43127f26ac5c47c998f0c6b975a018d
Alexandre Pujol <alexandre@pujol.io>
2021-05-16 16:11:08 +0100

    Release pass-import 3.2

--- pass_import/__about__.py
+++ pass_import/__about__.py
@@ -13,7 +13,7 @@ __summary__ = ('A pass extension for importing data from most of '
                'the existing password manager.')
 __uri__ = 'https://github.com/roddhjav/pass-import'
 
-__version__ = '3.1'
+__version__ = '3.2'
 
 __author__ = 'Alexandre Pujol'
 __email__ = 'alexandre@pujol.io'
