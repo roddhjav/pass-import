dcb1b969912a9c3b2c531a440083d0ff96dc6bc1
stf <7o5rfu92t@ctrlc.hu>
2022-09-03 13:04:42 +0000

    changed according to review comments

--- pass_import/managers/sphinx.py
+++ pass_import/managers/sphinx.py
@@ -27,10 +27,7 @@ class Sphinx(PasswordExporter):
     name = 'sphinx'
     default = True
     url = 'https://github.com/stef/pwdsphinx/'
-    cap = Cap.EXPORT
     name = 'sphinx'
-    reused = {}
-    weak = 0
 
     def exist(self):
         """Nothing to do."""
@@ -58,19 +55,6 @@ class Sphinx(PasswordExporter):
             host = entry.pop('path','')
         #print("user", user, "host", host, "pwd", pwd)
 
-        q = sphinx.zxcvbn(pwd)
-        if q['score']<4:
-            self.weak+=1
-            print(f"Password for {user}@{host} is weak consider changing it asap with sphinx.")
-            #print(q)
-        if pwd in self.reused:
-            print(f"Password for {user}@{host} is reused also for:")
-            for id in self.reused[pwd]:
-                print(f"\t{id}")
-            self.reused[pwd].append(f"{user}@{host}")
-        else:
-            self.reused[pwd]=[f"{user}@{host}"]
-
         try: sphinx.bin2pass.pass2bin(pwd, None)
         except OverflowError:
             raise PMError(f"Failed to import password for {user}@{host} to sphinx: too big.")
@@ -91,11 +75,5 @@ class Sphinx(PasswordExporter):
 
     def close(self):
         self.masterpass = None
-        if self.weak>0:
-            print(f"exported {self.weak} weak passwords")
-        reused = sum(1 for id in self.reused.values() if len(id)>1)
-        if reused > 0:
-            print(f"exported {reused} password which were not unique")
-        self.reused = {}
 
 register_managers(Sphinx)
