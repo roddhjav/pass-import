f001e8e051b1085f92ce9b188cdb9b60c6dc24a0
Alexandre Pujol <alexandre@pujol.io>
2022-09-07 22:49:42 +0100

    style: linter fix on sphinx tests.

--- tests/exports/test_sphinx.py
+++ tests/exports/test_sphinx.py
@@ -5,52 +5,63 @@
 
 from unittest.mock import patch
 
-from pass_import.managers.sphinx import Sphinx
-from pwdsphinx.sphinx import RULE_SIZE
-from pass_import.errors import PMError
-from pwdsphinx import sphinx as pwdsphinx
-
 import tests
+from pass_import.errors import PMError
+from pass_import.managers.sphinx import Sphinx
+from pwdsphinx import sphinx as pwdsphinx
+from pwdsphinx.sphinx import RULE_SIZE
+
 
 class MockCreateSock:
+    """Mock Sphinx response."""
     state = 0
     actions = (
-        ('send', 65),              # recv 0x00|id[32]|alpha[32]
-                                   # send beta[32]
-        ('recv', b'\xaa\x16\xc7?\x81\xf1\xf9qw\x00\x0b\x1b\xdb\x0bA\xcf\x19\xce\xeb_O\xe3\x99\xe5G\xb8M\xa9\x1a\xbcKM'),
+        ('send', 65),               # recv 0x00|id[32]|alpha[32]
+                                    # send beta[32]
+        ('recv', b'\xaa\x16\xc7?\x81\xf1\xf9qw\x00\x0b\x1b\xdb\x0bA\xcf\x19'
+         b'\xce\xeb_O\xe3\x99\xe5G\xb8M\xa9\x1a\xbcKM'),
         ('send', 32+RULE_SIZE+64),  # pubkey, rule, signature
-        ('send', 32+64),           # id + signature
+        ('send', 32+64),            # id + signature
         ('recv', b"\x00\x00"),
-        ('send', 0),               # pk[32], size[2], pkt[size], signature[64]
+        ('send', 0),                # pk[32], size[2], pkt[size], signature[64]
         ('recv', 'ok')
     )
 
     def send(self, pkt):
         action = self.actions[self.state]
         assert action[0] == 'send'
-        if action[1]: assert len(pkt) == action[1]
-        self.state+=1
+        if action[1]:
+            assert len(pkt) == action[1]
+        self.state += 1
 
     def recv(self, size):
         action = self.actions[self.state]
         assert action[0] == 'recv'
         assert size == len(action[1])
-        self.state+=1
+        self.state += 1
         return action[1]
 
     def close(self):
         return
 
+
 def challenge(_):
     return (
-        b"\xaeiOo\xda\x8d54#\x02\x03uBXr\xdfSie\xbb\xfb?_\x07Z\xb7b'\xd1n\xde\x00",
-        b'\xdc\xb2\x9c\xf6\x06\xa4\x82\x18\xc2\xc4$\xed\x01(\x12\x16\xeb@\xdeOr\xca\x14\xf7\xf6\xb7\x04\x86B\xdaoW'
+        b"\xaeiOo\xda\x8d54#\x02\x03uBXr\xdfSie\xbb\xfb?_\x07Z\xb7b'\xd1n"
+        b"\xde\x00",
+        b'\xdc\xb2\x9c\xf6\x06\xa4\x82\x18\xc2\xc4$\xed\x01(\x12\x16\xeb@'
+        b'\xdeOr\xca\x14\xf7\xf6\xb7\x04\x86B\xdaoW'
     )
-def finish(w,x,y,z):
-    return b'\\\xd6{\xed\xc70\xa1\xb3\xca\xeb\x0b\x91p\x11\x15\xe9P\xdfx\xe1\x8f\x12\x96[\x05\xed\x8c\xad\xb8\xeb\xd7Z'
+
+
+def finish(w, x, y, z):
+    return b'\\\xd6{\xed\xc70\xa1\xb3\xca\xeb\x0b\x91p\x11\x15\xe9P\xdfx\xe1'
+    b'\x8f\x12\x96[\x05\xed\x8c\xad\xb8\xeb\xd7Z'
+
 
 def connect():
     return MockCreateSock()
+
 
 pwdsphinx.connect = connect
 pwdsphinx.sphinxlib.challenge = challenge
@@ -59,6 +70,7 @@ pwdsphinx.sphinxlib.finish = finish
 
 class TestExportSphinx(tests.Test):
     """Test sphinx general features."""
+
     def setUp(self):
         self.sphinx = Sphinx(self.prefix)
 
@@ -69,6 +81,7 @@ class TestExportSphinx(tests.Test):
     def test_sphinx_isvalid(self):
         """Testing: isvalid."""
         self.assertTrue(self.sphinx.isvalid())
+
 
 class TestExportSphinxInsert(tests.Test):
     """Test Sphinx insert features."""
@@ -194,7 +207,10 @@ class TestExportSphinxInsert(tests.Test):
         """Testing: sphinx insert too long."""
         pw.return_value = self.masterpassword
         entry = {
-            'password': 'lnqYm3ZWtm44jle5q3fdvrprmaahozexy2pi44jle5q3fdvrprmaahozexy2pi44jle5q3fdvrprmaahozexy2pilnqYm3ZWtm44jle5q3fdvrprmaahozexy2pi44jle5q3fdvrprmaahozexy2pi44jle5q3fdvrprmaahozexy2pipassword',
+            'password': 'lnqYm3ZWtm44jle5q3fdvrprmaahozexy2pi44jle5q3fdvrprmaa'
+                        'hozexy2pi44jle5q3fdvrprmaahozexy2pilnqYm3ZWtm44jle5q3'
+                        'fdvrprmaahozexy2pi44jle5q3fdvrprmaahozexy2pi44jle5q3f'
+                        'dvrprmaahozexy2pipassword',
             'login': 'lnqYm3ZWtm',
             'url': 'https://twitter.com',
             'website': 'https://pujol.io',
