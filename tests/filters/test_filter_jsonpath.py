1a0866520fc2fe98464a882f08410f9e88efc328
Alexandre Pujol <alexandre@pujol.io>
2024-02-14 14:01:11 +0000

    chore: minor code linting.

--- tests/filters/test_filter_jsonpath.py
+++ tests/filters/test_filter_jsonpath.py
@@ -2,13 +2,13 @@
 # pass-import - test suite
 # Copyright (C) 2017-2020 Alexandre PUJOL <alexandre@pujol.io>.
 #
-from pass_import.__main__ import pass_export, setup
+
+from pass_import.__main__ import pass_export
 from pass_import.core import Cap
-from pass_import.errors import PMError
 from pass_import.manager import PasswordImporter, PasswordExporter
-from pass_import.managers.csv import GenericCSV
-import tests
 from pass_import.tools import Config
+
+import tests
 
 
 class MockManager(PasswordImporter, PasswordExporter):
@@ -47,18 +47,19 @@ CONF = {
 
 }
 
+
 class TestFilter(tests.Test):
     """Test --filter features."""
 
     def setUp(self):
         """Setup a new keepass repository."""
         self.conf = Config()
-        self.conf['droot']="/tmp/not/used/but/required"
-        self.conf['out']="/tmp/not/used/but/required"
-        self.conf['clean']=False
-        self.conf['convert']=False
-        self.conf['pwned']=False
-        self.conf['dry_run']=False
+        self.conf['droot'] = "/tmp/not/used/but/required"
+        self.conf['out'] = "/tmp/not/used/but/required"
+        self.conf['clean'] = False
+        self.conf['convert'] = False
+        self.conf['pwned'] = False
+        self.conf['dry_run'] = False
         self.exporter = MockManager(self)
 
     def test_no_filter_on_non_empty_data_source(self):
@@ -84,7 +85,6 @@ class TestFilter(tests.Test):
         self.assertTrue(len(paths_imported) == 1)
         self.assertTrue(len(paths_imported) == len(paths_exported))
 
-
     def test_filter_on_non_empty_data_source(self):
         """Testing: export some entries when filter provided using non
         empty data source."""
@@ -108,4 +108,3 @@ class TestFilter(tests.Test):
         self.assertTrue(len(paths_imported) == 2)
         self.assertTrue(len(paths_exported) == 1)
         self.assertTrue(data[0]['login'] in paths_exported[0])
-
